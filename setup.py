1e5de7f8fa4f3f2dfc9aa1f410119f127c707520
im-vishal <iamvirtualvishal@gmail.com>
2024-07-04 10:40:46 +0530

    requirements added

--- setup.py
+++ setup.py
@@ -1,10 +1,29 @@
-from setuptools import find_packages, setup
+import setuptools
 
-setup(
-    name='src',
-    packages=find_packages(),
-    version='0.1.0',
-    description='An ML project with MLOps best practices.',
-    author='Vishal Pandey',
-    license='',
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "e2e-mlops-project"
+AUTHOR_USER_NAME = "im-vishal"
+SRC_REPO = "mlProject"
+AUTHOR_EMAIL = "iamvirtualvishal@gmail.com"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for ml app",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
 )
